eee1caa7ff7c923603583537857b6058f75bb548
Rajesh Narayana Rao <mailrbn@gmail.com>
2019-05-25 09:32:23 +1000

    25May2019

---
+++ ittertool1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 10:51:32 2019
+
+@author: rajeshnarayanarao
+"""
+
+import itertools
+
+counter = itertools.count()
+
+#for num in counter:
+#    print(num)
+
+#print(next(counter))
+
+#data = [100, 200, 300, 400]
+#daily_data = list(zip(itertools.count(), data))
+#
+#print(daily_data)
+#
+#daily_data = list(itertools.zip_longest(range(10), data))
+#
+#print(daily_data)
+
+#squares = map(pow, range(10), itertools.repeat(3))
+
+#
+
+print(list(squares))
